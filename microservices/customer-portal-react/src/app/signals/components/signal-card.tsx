ba88ee1fc16900831297a276f95401565dddbd0c
Ilias Hossain <mdilias.hossain@henkel.com>
2023-05-28 08:03:13 +0200

    [added] UI for display signals

--- microservices/customer-portal-react/src/app/signals/components/signal-card.tsx
+++ microservices/customer-portal-react/src/app/signals/components/signal-card.tsx
@@ -3,11 +3,6 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import localeEn from "dayjs/locale/en";
 
-const sizeclassNamees = {
-  height: "h-32",
-  width: "w-32",
-};
-
 interface SignalCardProps {
   signal: Signal;
 }
@@ -24,7 +19,7 @@ const intervals = [
 
 const daysago = (postDate) => {
   dayjs.extend(relativeTime).locale(localeEn)
-  var fromNowOn = dayjs(postDate).fromNow();
+  const fromNowOn = dayjs(postDate).fromNow();
   return (fromNowOn)
 };
 
@@ -32,7 +27,7 @@ export default function SignalCard({ signal }: SignalCardProps) {
   return (
     <li className="flex justify-between gap-x-6 py-5">
       <div className="flex gap-x-4">
-        <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src="/bitcoin.png" alt="" />
+        <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={signal.ticker + ".png"} alt="" />
         <div className="min-w-0 flex-auto">
           <p className="text-sm font-semibold leading-6 text-gray-900">{signal.ticker}</p>
           <p className="mt-1 truncate text-xs leading-5 text-gray-500">{signal.algo}</p>
@@ -40,7 +35,7 @@ export default function SignalCard({ signal }: SignalCardProps) {
       </div>
       {intervals.map((interval) => (
         <div key={interval.id} className="hidden sm:flex sm:flex-col sm:items-end">
-          <p className="text-sm leading-6 text-gray-900">
+          <p className="text-sm leading-6 text-gray-900 text-left">
             {interval.value}
             {signal.alertByInterval[interval.name] ?
               signal.alertByInterval[interval.name]?.tradeType === 'buy' ? <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
@@ -50,7 +45,7 @@ export default function SignalCard({ signal }: SignalCardProps) {
               </span> : null
             }
           </p>
-          <p className="mt-1 text-xs leading-5 text-gray-500">
+          <p className="mt-1 text-xs leading-5 text-gray-500 text-left">
             {signal.alertByInterval[interval.name] ?
               <time>{daysago(signal.alertByInterval[interval.name]?.timestamp)}</time> :
               "(Not Defined)"
--- microservices/customer-portal-react/src/app/signals/signals.tsx
+++ microservices/customer-portal-react/src/app/signals/signals.tsx
@@ -3,62 +3,91 @@ import styles from './signals.module.css';
 
 import SignalCard from './components/signal-card'
 import { Signal } from './domain/signal';
-import { ChangeEvent, useEffect, useState } from 'react';
+import {  useEffect, useState } from 'react';
 import *  as  Realm from "realm-web";
-import { isGeneratorFunction } from 'util/types';
 
 /* eslint-disable-next-line */
 export interface SignalsProps { }
 
 export function Signals() {
-  const [signals, setSignals] = useState<Signal[]>([]);
+  const [signals, setSignals] = useState<Signal[]>();
   const [user, setUser] = useState<unknown>();
+  
   const app = new Realm.App({ id: "data-lsbmb" });
-  // useEffect(() => {
-  //   async function loadSignalSummary() {
-  //     const res = await fetch('https://eu-central-1.aws.data.mongodb-api.com/app/data-lsbmb/endpoint/get_signal_summary');
-  //     const result = await res.json();
 
-  //     if (result.error) {
-  //       setSignals([]);
-  //     } else {
-  //       setSignals(result);
-  //     }
-  //   }
+  const loadSignalSummary = async () => {
+    const res = await fetch('https://eu-central-1.aws.data.mongodb-api.com/app/data-lsbmb/endpoint/get_signal_summary');
+    const result = await res.json();
 
-  //   loadSignalSummary();
+    if (result.error) {
+      setSignals([]);
+    } else {
+      setSignals(result);
+    }
 
-  //   setInterval(async () => {
-  //     loadSignalSummary();
-  //   }, 10000);
+   //
+  } 
 
-  // }, []);
+  const setupSSEConnection = async () => {
+
+    if(!signals)
+      return;
+    // Authenticate anonymously
+    const user = await app.logIn(Realm.Credentials.anonymous());
+    setUser(user);
+
+    // Connect to the database
+    const mongodb = app.currentUser?.mongoClient("Cluster0");
+    const collection = mongodb?.db("ProjectZero").collection("TradeAlertSummary");
+
+    // Everytime a change happens in the stream, add it to the list of events
+    if (collection) {
+      for await (const change of collection.watch(
+        { fullDocument : "updateLookup" })) {
+          if(change.operationType === 'update' || change.operationType === 'insert')
+          {
+            let signal = signals?.find(s => s._id ===  change.fullDocument._id.toString());
+
+            if(!signal){
+              setSignals(signalss  => [...signals, change.fullDocument as Signal]);
+              //signal = change.fullDocument as Signal
+              //setSignals(signals);
+            }
+            else{
+
+              const updatedSignal = change.fullDocument as Signal;
+
+              signal = updatedSignal;
+              setSignals(prevArray => {
+                return prevArray?.map(obj => {
+                  if (obj._id === updatedSignal?._id.toString()) {
+                    return { ...obj, ...signal };
+                  }
+                  return obj;
+                });
+              });
+            }
+          }
+      }
+    }
+   }
+
+  useEffect(() => {
+    
+    loadSignalSummary();
+
+  }, []);
 
 
   // This useEffect hook will run only once when the page is loaded
 
   useEffect(() => {
-    const login = async () => {
-      // Authenticate anonymously
-      const user = await app.logIn(Realm.Credentials.anonymous());
-      setUser(user);
-
-      // Connect to the database
-      const mongodb = app.currentUser?.mongoClient("mongodb-atlas");
-      const collection = mongodb?.db("ProjectZero").collection("TradeAlertSummary");
-
-      // Everytime a change happens in the stream, add it to the list of events
-      if (collection) {
-        for await (const change of collection.watch()) {
-          setSignals(signals  => [...signals, change.valueOf() as Signal]);
-        }
-      }
-    }
-    login();
-  }, []);
+console.log('omg')
+    setupSSEConnection();
+  }, [signals]);
 
   return (
-    <ul role="list" className="divide-y divide-gray-100">
+    <ul className="divide-y divide-gray-100">
       {signals?.map((signal) => <SignalCard key={signal._id} signal={signal} />)}
     </ul>
   )
